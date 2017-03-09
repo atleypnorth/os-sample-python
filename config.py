5665a4bf35dc955c4adfe0a0f7e313ba036f54cf
unknown <patrick@shimi.co.uk>
2017-03-09 22:23:27 +0000

    add mysql connect

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ import os
 
 SECRET_KEY = 'top-secret'
 if os.environ.get('MYSQL_USER') is not None:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://%s:%s@%s:%s/%s' % (os.environ['MYSQL_USER'],
-                                                                  os.environ['MYSQL_PASSWORD'],
-                                                                  os.environ['MYSQL_SERVICE_HOST'],
-                                                                  os.environ['MYSQL_SERVICE_PORT'],
-                                                                  os.environ['MYSQL_DATABASE'])
+#    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://%s:%s@%s:%s/%s' % (os.environ['MYSQL_USER'],
+#                                                                  os.environ['MYSQL_PASSWORD'],
+#                                                                  os.environ['MYSQL_SERVICE_HOST'],
+#                                                                  os.environ['MYSQL_SERVICE_PORT'],
+#                                                                  os.environ['MYSQL_DATABASE'])
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
 
