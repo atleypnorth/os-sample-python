28c2eaf33da02f998160b818820bbe54d4f0f7f6
unknown <patrick@shimi.co.uk>
2017-03-09 10:47:03 +0000

    one process

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ import os
 
 SECRET_KEY = 'top-secret'
 db_path = os.environ.get('DB_PATH','db.sqlite')
+db_path = 'db.sqlite'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///%s' % db_path)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
